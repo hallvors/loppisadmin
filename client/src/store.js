3a893a33873dc475aff1017a56cfe8aacea81634
Hallvord Reiar Michaelsen Steen <hallvord@hallvord.com>
2019-08-18 21:31:10 +0200

    Store drivers in local storage between sessions

--- client/src/store.js
+++ client/src/store.js
@@ -2,3 +2,14 @@ import { writable } from 'svelte/store';
 
 export const drivers = writable([]);
 export const jobs = writable([]);
+
+// contents of drivers are synced to localStorage
+if (typeof localStorage !== 'undefined') {
+	let existingData = localStorage.getItem('drivers');
+	if (existingData) {
+		drivers.set(JSON.parse(existingData));
+	}
+	let unsub = drivers.subscribe(data => {
+		localStorage.setItem('drivers', JSON.stringify(data));
+	});
+}
