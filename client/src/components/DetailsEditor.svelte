d3bd1310b235e965198a6352cc7787cf0b12941f
Hallvord Reiar Michaelsen Steen <hallvord@hallvord.com>
2019-08-19 23:38:03 +0200

    make job size editable

--- client/src/components/DetailsEditor.svelte
+++ client/src/components/DetailsEditor.svelte
@@ -9,19 +9,16 @@ let number = job.telefonnummer;
 let info = job.informasjonomloppene;
 let time = job.hentetidspunktkryssavsåmangedukan.split(/,\s*/g);
 let address = job.adresseforhenting;
-let descs = {
-	number: 'Mobilnummer',
-	time: 'Hentetidspunkt',
-	info: 'Om loppene', 
-	address: 'Adresse for henting'
-}
+let size = job.størrelse;
+const bigStr = 'Store ting (vare-bil eller tilhenger kreves ved henting)';
 
 function update() {
 	dispatch('update', {
 		telefonnummer: number,
 		adresseforhenting: address,
 		informasjonomloppene: info,
-		hentetidspunktkryssavsåmangedukan: time.join(', ')
+		hentetidspunktkryssavsåmangedukan: time.join(', '),
+		størrelse: size,
 	});
 }
 
@@ -45,7 +42,7 @@ function update() {
 	span:first-child {
 		width: 30%;
 	}
-	span * {
+	span>input, span>textarea, span>select {
 		width: 100%;
 	}
 	span button {
@@ -56,25 +53,52 @@ function update() {
 	}
 	textarea {height: 100px;}
 	input, textarea, select {font-size: 1em;}
+	span label {
+		display: block;
+		width: 45%;
+		float: left;
+	}
+	span label img {
+		vertical-align: bottom;
+	}
 </style>
 <form  on:submit|preventDefault={e => update()}>
 		<p>
-			<span>{descs.number}:</span><span><input bind:value={number} inputmode="tel"></span>
+			<span>Mobilnummer:</span><span><input bind:value={number} inputmode="tel"></span>
 		</p>
 		<p>
-			<span>{descs.address}:</span><span><textarea bind:value={address}></textarea></span>
+			<span>Adresse for henting:</span><span><textarea bind:value={address}></textarea></span>
 		</p>
 		<p>
-			<span>{descs.info}:</span><span><textarea bind:value={info}></textarea></span>
+			<span>Om loppene:</span><span><textarea bind:value={info}></textarea></span>
 		</p>
 		<p>
-			<span>{descs.time}</span><span>
+			<span>Hentetidspunkt:</span><span>
 				<select multiple bind:value={time}>
 					<!--<option>Mandag kveld</option>-->
 					<option>Tirsdag kveld</option>
 					<option>Onsdag kveld</option>
 					<option>Torsdag kveld</option>
 				</select>
+			</span>
+		</p>
+		<p>
+			<span>Type bil:</span>
+			<span>
+				<label><input
+					type="radio"
+					name="size"
+					bind:group={size}
+					value={bigStr}>
+						<img src="/images/bigcar.png" alt="stor bil" width="36">
+				</label>
+				<label><input
+					type="radio"
+					name="size"
+					bind:group={size}
+					value={''}>
+						<img src="/images/smallcar.png" alt="liten bil" width="36">
+				</label>
 			</span>
 		</p>
 		<p><span></span>
