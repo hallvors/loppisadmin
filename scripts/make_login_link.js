3211f233334bd704880d442bd1cb24174e44ce09
Hallvord R. M. Steen <hallvord@minus.no>
2022-08-19 10:54:23 +0200

    Let login links last for longer (12 days)

--- scripts/make_login_link.js
+++ scripts/make_login_link.js
@@ -4,7 +4,7 @@ const authTokenSecret = env.nconf.get('site:authTokenSecret');
 const baseUrl = env.nconf.get('site:baseUrl');
 const jwt = require('jsonwebtoken');
 
-const token = jwt.sign({loppislogin: true}, authTokenSecret, {expiresIn: '2 days'});
+const token = jwt.sign({loppislogin: true}, authTokenSecret, {expiresIn: '12 days'});
 console.log('For å logge inn, gå til:');
 console.log(baseUrl + '/api/login?token=' + encodeURIComponent(token));
 console.log('\nMerk: bare gyldig i to dager.')
